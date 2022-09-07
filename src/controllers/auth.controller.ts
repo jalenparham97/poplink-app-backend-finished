6da7e1c6b48d2d8c9df7d2bc3e5759351f6fc38f
Jalen Parham <jalenparham97@gmail.com>
2022-09-06 21:34:11 -0400

    Adding simple error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,12 +2,22 @@ import { Request, Response } from 'express';
 import { authService } from '../services/auth.service';
 
 export async function login(req: Request, res: Response) {
-  const user = await authService.login(req.query.uid as string);
-  if (!user) return res.status(404).json('User Not Found');
-  return res.status(200).json(user);
+  try {
+    const user = await authService.login(req.query.uid as string);
+    if (!user) return res.status(404).json('User Not Found');
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
 
 export async function signUp(req: Request, res: Response) {
-  const user = await authService.signup(req.body);
-  return res.status(200).json(user);
+  try {
+    const user = await authService.signup(req.body);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -2,45 +2,89 @@ import { Request, Response } from 'express';
 import { profileService } from '../services/profile.service';
 
 export async function getProfiles(req: Request, res: Response) {
-  const profiles = await profileService.getProfiles();
-  return res.status(200).json(profiles);
+  try {
+    const profiles = await profileService.getProfiles();
+    return res.status(200).json(profiles);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
 
 export async function getUserProfiles(req: Request, res: Response) {
-  const links = await profileService.getUserProfiles(
-    req.query.userId as string
-  );
-  return res.status(200).json(links);
+  try {
+    const links = await profileService.getUserProfiles(
+      req.query.userId as string
+    );
+    return res.status(200).json(links);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 }
 
 export async function getProfile(req: Request, res: Response) {
-  const profile = await profileService.getProfile(req.params.id);
-  if (!profile) return res.status(404).json('Profile Not Found');
-  return res.status(200).json(profile);
+  try {
+    const profile = await profileService.getProfile(req.params.id);
+    if (!profile) return res.status(404).json('Profile Not Found');
+    return res.status(200).json(profile);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
 
 export async function getProfileByUsername(req: Request, res: Response) {
-  const profile = await profileService.getProfileByUsername(
-    req.query.profileUsername as string
-  );
-  if (!profile) return res.status(404).json('Profile Not Found');
-  return res.status(200).json(profile);
+  try {
+    const profile = await profileService.getProfileByUsername(
+      req.query.profileUsername as string
+    );
+    if (!profile) return res.status(404).json('Profile Not Found');
+    return res.status(200).json(profile);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
 
 export async function addProfile(req: Request, res: Response) {
-  const newProfile = await profileService.addProfile(req.body);
-  return res.status(201).json(newProfile);
+  try {
+    const newProfile = await profileService.addProfile(req.body);
+    return res.status(201).json(newProfile);
+  } catch (error: any) {
+    console.log(error);
+    if (error.codeName === 'DuplicateKey') {
+      return res
+        .status(400)
+        .json({ message: 'Profile username already exists' });
+    }
+    return res.status(500).json(error);
+  }
 }
 
 export async function updateProfile(req: Request, res: Response) {
-  const updatedProfile = await profileService.updateProfile(
-    req.params.id,
-    req.body
-  );
-  return res.status(200).json(updatedProfile);
+  try {
+    const updatedProfile = await profileService.updateProfile(
+      req.params.id,
+      req.body
+    );
+    return res.status(200).json(updatedProfile);
+  } catch (error: any) {
+    console.log(error);
+    if (error.codeName === 'DuplicateKey') {
+      return res
+        .status(400)
+        .json({ message: 'Profile username already exists' });
+    }
+    return res.status(500).json(error);
+  }
 }
 
 export async function deleteProfile(req: Request, res: Response) {
-  await profileService.deleteProfile(req.params.id);
-  res.sendStatus(204);
+  try {
+    await profileService.deleteProfile(req.params.id);
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 }
